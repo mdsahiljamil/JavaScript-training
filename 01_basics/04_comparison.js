3aad7099d8f88d449a8c25efe6eea5f27c425426
mdsahiljamil <mdsahiljamil2000@gmail.com>
2024-01-09 21:47:53 +0530

    comparison of datatype

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// **this below comparison are basic**
+/*
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1); */
+
+// console.log("2">1); // *the complier will automatically convert string into number
+// console.log("03">"6")
+
+//  ***Always avoid this type of comparison***
+//their is a diff. b/w equality check and comparison 
+// console.log(null > 0); // false
+// console.log(null == 0); // false
+// console.log(null >= 0); //true
+// and in the case of undefined , it show false in everyone 
+
+// **("==="strick comparison-its compare inputs as well as their "datatypes")
+// console.log("2"===0); // => false
+
