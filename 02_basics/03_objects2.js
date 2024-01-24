80143c48e0a34ce3918e689404cbb3e482a680d5
mdsahiljamil <mdsahiljamil2000@gmail.com>
2024-01-24 12:20:32 +0530

    object and json Api

--- 02_basics/03_objects2.js
+++ 02_basics/03_objects2.js
@@ -6,7 +6,7 @@ tinderUser.id="123abc"
 tinderUser.name= "sahil"
 tinderUser.isloggedIn = false
  
-console.log(tinderUser);
+// console.log(tinderUser);
 
 const regularUser ={
     email: "sahil@gmail.com",
@@ -18,8 +18,8 @@ const regularUser ={
     }
 }
 
-console.log(regularUser.email);
-console.log(regularUser.full_name.userFullName.first_name);
+// console.log(regularUser.email);
+// console.log(regularUser.full_name.userFullName.first_name);
 
 //* adding two objects
 
@@ -30,7 +30,7 @@ const obj3 = {5:"a",6:"b"}
 // const obj4 = {obj1,obj2}// can't give desire objects
 // const obj4= Object.assign({},obj1,obj2,obj3)
 const obj4={...obj1,...obj2,...obj3}
-console.log(obj4)
+// console.log(obj4)
 
 //* many objects in an array
 
@@ -48,11 +48,44 @@ const User =[
         email:"xyz3@gmail.com"
     },
 ]
-console.log(User[0].email)//[index value]
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));// key will display in an array
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));// op will get array into array
+// console.log(User[0].email)//[index value]
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));// key will display in an array
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));// op will get array into array
 
 // to known some specific "key value" are present in an object or not
-console.log(tinderUser.hasOwnProperty('sahil'))
+// console.log(tinderUser.hasOwnProperty('sahil'))
+
+//* object destructure {} & (javascript object notation)JSON API
+
+const course ={
+    courseName:"javascript",
+    price:999,
+    courseTeacher:"hitesh",
+
+}
+// course.courseName // this method is good but we see anther also
+
+// const{courseName:cName}=course
+// console.log(courseName);
+console.log(cName);
+
+//* json API
+/* // in json both key & value are written in string
+{
+    "name":"sahil",
+    "courseName":"learning javascript"
+    "price":"free"
+    "courseId": 234691500
+    "isloggedIn": false
+}
+ # sometime we get API in array format like this
+ [
+    {};
+    {};
+    {};
+    {};
+ ]
+*/
+// WE use json formatter to understand the format of json
